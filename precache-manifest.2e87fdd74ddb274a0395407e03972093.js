7ea4583f46a870644fc928d9b213fbf67b142296
salgilbarana <ychcom@naver.com>
2020-02-24 02:38:00 +0900

    Updates

---
+++ precache-manifest.2e87fdd74ddb274a0395407e03972093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1f48e4d7578d3b57cec94b1b85d3a1c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "f1ed9f30a99cca56c335",
+    "url": "/movie_app/static/css/main.9ff507a2.chunk.css"
+  },
+  {
+    "revision": "063f3751b77f4e1e989b",
+    "url": "/movie_app/static/js/2.e746f176.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.e746f176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ed9f30a99cca56c335",
+    "url": "/movie_app/static/js/main.d6b5bf20.chunk.js"
+  },
+  {
+    "revision": "afdf0258d99bd56019d8",
+    "url": "/movie_app/static/js/runtime-main.dd5bb62c.js"
+  }
+]);
